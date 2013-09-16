b9604656ba37de4454acb3262106b99e5a3e16eb
Brandon Wulf <bwulf@labattfood.com>
2013-09-15 23:48:26 -0500

    Everything works. Removed blank lines and added a custom link type.

--- bp-direct-menu/inc/backend-noajax.inc.php
+++ bp-direct-menu/inc/backend-noajax.inc.php
@@ -1,85 +1,82 @@
-<?php
-if( !defined( 'ABSPATH' ) )
-	die( 'Cheatin\' uh?' );
-
-/* Add a metabox in admin menu page */
-add_action('admin_head-nav-menus.php', 'bpdm_add_nav_menu_metabox');
-function bpdm_add_nav_menu_metabox() {
-	add_meta_box( 'bpdm', __( 'Login/Logout links' ) . ' v' . BPDIRECTMENU_VERSION, 'bpdm_nav_menu_metabox', 'nav-menus', 'side', 'default' );
-}
-
-/* The metabox code : Awesome code stolen from screenfeed.fr (GregLone) Thank you mate. */
-function bpdm_nav_menu_metabox( $object )
-{
-	global $nav_menu_selected_id;
-
-	$elems = array( '#bpdmlogin#' => __( 'Log In' ), '#bpdmlogout#' => __( 'Log Out' ), '#bpdmloginout#' => __( 'Log In' ).'|'.__( 'Log Out' ), '#bpdmregister#' => __( 'Register' ) );
-	class bpdmlogItems {
-		public $db_id = 0;
-		public $object = 'bpdmlog';
-		public $object_id;
-		public $menu_item_parent = 0;
-		public $type = 'custom';
-		public $title;
-		public $url;
-		public $target = '';
-		public $attr_title = '';
-		public $classes = array();
-		public $xfn = '';
-	}
-
-	$elems_obj = array();
-	foreach ( $elems as $value => $title ) {
-		$elems_obj[$title] = new bpdmlogItems();
-		$elems_obj[$title]->object_id	= esc_attr( $value );
-		$elems_obj[$title]->title		= esc_attr( $title );
-		$elems_obj[$title]->url			= esc_attr( $value );
-	}
-
-	$walker = new Walker_Nav_Menu_Checklist( array() );
-	?>
-	<div id="login-links" class="loginlinksdiv">
-
-		<div id="tabs-panel-login-links-all" class="tabs-panel tabs-panel-view-all tabs-panel-active">
-			<ul id="login-linkschecklist" class="list:login-links categorychecklist form-no-clear">
-				<?php echo walk_nav_menu_tree( array_map( 'wp_setup_nav_menu_item', $elems_obj ), 0, (object)array( 'walker' => $walker ) ); ?>
-			</ul>
-		</div>
-
-		<p class="button-controls">
-			<span class="list-controls hide-if-no-js">
-				<a href="javascript:void(0);" class="help" onclick="jQuery( '#help-login-links' ).toggle();"><?php _e( 'Help' ); ?></a>
-				<span class="hide-if-js" id="help-login-links"><br /><a name="help-login-links"></a>
-					<?php
-					if( get_locale() == 'fr_FR' ) { // Light L10N
-						echo '&#9725; Vous pouvez ajouter une page de redirection apr&egrave;s le login/logout du membre en ajoutant simplement le lien relatif apr&egrave;s le mot cl&eacute; dans le lien, exemple <code>#bpdmloginout#index.php</code>.';
-						echo '<br />&#9725; Vous pouvez aussi ajouter <code>%actualpage%</code> pour que la redirection soit faite sur la page en cours, exemple : <code>#bpdmloginout#%actualpage%</code>.';
-					}else{
-						echo '&#9725; You can add a redirection page after the user\'s login/logout simply adding a relative link after the link\'s keyword, example <code>#bpdmloginout#index.php</code>.';
-						echo '<br />&#9725; You can also add <code>%actualpage%</code> to redirect the user on the actual visited page, example : <code>#bpdmloginout#%actualpage%</code>.';
-					}
-					
-					?>
-				</span>
-			</span>
-
-			<span class="add-to-menu">
-				<input type="submit"<?php disabled( $nav_menu_selected_id, 0 ); ?> class="button-secondary submit-add-to-menu right" value="<?php esc_attr_e('Add to Menu'); ?>" name="add-login-links-menu-item" id="submit-login-links" />
-				<span class="spinner"></span>
-			</span>
-		</p>
-
-	</div>
-	<?php
-}
-
-/* Modify the "type_label" */
-add_filter( 'wp_setup_nav_menu_item', 'bpdm_nav_menu_type_label' );
-function bpdm_nav_menu_type_label( $menu_item )
-{
-	$elems = array( '#bpdmlogin#', '#bpdmlogout#', '#bpdmloginout#', '#bpdmregister#' );
-	if ( isset($menu_item->object, $menu_item->url) && $menu_item->object == 'custom' && in_array($menu_item->url, $elems) )
-		$menu_item->type_label = ( get_locale() == 'fr_FR' ? 'Connexion' : 'Connection' );
-
-	return $menu_item;
+<?php
+if( !defined( 'ABSPATH' ) )
+	die( 'Cheatin\' uh?' );
+
+/* Add a metabox in admin menu page */
+add_action('admin_head-nav-menus.php', 'bpdm_add_nav_menu_metabox');
+function bpdm_add_nav_menu_metabox() {
+	add_meta_box( 'bpdm', __( 'BuddyPress Direct Links' ) . ' v' . BPDIRECTMENU_VERSION, 'bpdm_nav_menu_metabox', 'nav-menus', 'side', 'default' );
+}
+
+/* The metabox code : Awesome code stolen from screenfeed.fr (GregLone) Thank you mate. */
+function bpdm_nav_menu_metabox( $object )
+{
+	global $nav_menu_selected_id;
+
+	$elems = array( '#bpdmlogin#' => __( 'Log In' ), 
+					'#bpdmlogout#' => __( 'Log Out' ), 
+					'#bpdmloginout#' => __( 'Log In' ).'|'.__( 'Log Out' ), 
+					'#bpdmregister#' => __( 'Register' ) ,
+					'#bpdmcustom#' => __( 'Custom Direct Link' )
+				   );
+	class bpdmlogItems {
+		public $db_id = 0;
+		public $object = 'bpdmlog';
+		public $object_id;
+		public $menu_item_parent = 0;
+		public $type = 'custom';
+		public $title;
+		public $url;
+		public $target = '';
+		public $attr_title = '';
+		public $classes = array();
+		public $xfn = '';
+	}
+
+	$elems_obj = array();
+	foreach ( $elems as $value => $title ) {
+		$elems_obj[$title] = new bpdmlogItems();
+		$elems_obj[$title]->object_id	= esc_attr( $value );
+		$elems_obj[$title]->title		= esc_attr( $title );
+		$elems_obj[$title]->url			= esc_attr( $value );
+	}
+
+	$walker = new Walker_Nav_Menu_Checklist( array() );
+	?>
+	<div id="bp-direct-links" class="bpdirectlinksdiv">
+		<div id="tabs-panel-bp-direct-links-all" class="tabs-panel tabs-panel-view-all tabs-panel-active">
+			<ul id="bp-direct-linkschecklist" class="list:bp-direct-links categorychecklist form-no-clear">
+				<?php echo walk_nav_menu_tree( array_map( 'wp_setup_nav_menu_item', $elems_obj ), 0, (object)array( 'walker' => $walker ) ); ?>
+			</ul>
+		</div>
+
+		<p class="button-controls">
+			<span class="list-controls hide-if-no-js">
+				<a href="javascript:void(0);" class="help" onclick="jQuery( '#help-bp-direct-links' ).toggle();"><?php _e( 'Help' ); ?></a>
+				<span class="hide-if-js" id="help-bp-direct-links"><br /><a name="help-bp-direct-links"></a>
+					<?php
+						echo '&#9725; You can add a redirection page after the user\'s login/logout simply adding a relative link after the link\'s keyword, example <code>#bpdmloginout#index.php</code>.';
+						echo '<br />&#9725; You can also add <code>%actualpage%</code> to redirect the user on the actual visited page, example : <code>#bpdmloginout#%actualpage%</code>.';
+						echo '<br />&#9725; For the custom link, put the url-piece after the keyword, example : <code>#bpdmcustom#/activity/friends/</code> will redirect to: <code>http://%site%/members/%username%/activity/friends/</code>';
+					?>
+				</span>
+			</span>
+
+			<span class="add-to-menu">
+				<input type="submit"<?php disabled( $nav_menu_selected_id, 0 ); ?> class="button-secondary submit-add-to-menu right" value="<?php esc_attr_e('Add to Menu'); ?>" name="add-bp-direct-links-menu-item" id="submit-bp-direct-links" />
+				<span class="spinner"></span>
+			</span>
+		</p>
+	</div>
+	<?php
+}
+
+/* Modify the "type_label" */
+add_filter( 'wp_setup_nav_menu_item', 'bpdm_nav_menu_type_label' );
+function bpdm_nav_menu_type_label( $menu_item )
+{
+	$elems = array( '#bpdmlogin#', '#bpdmlogout#', '#bpdmloginout#', '#bpdmregister#', '#bpdmcustom#' );
+	if ( isset($menu_item->object, $menu_item->url) && $menu_item->object == 'custom' && in_array($menu_item->url, $elems) )
+		$menu_item->type_label = ( 'BP Direct Link' );
+	return $menu_item;
 }
--- bp-direct-menu/inc/frontend-noajax.inc.php
+++ bp-direct-menu/inc/frontend-noajax.inc.php
@@ -1,109 +1,127 @@
-<?php
-if( !defined( 'ABSPATH' ) )
-	die( 'Cheatin\' uh?' );
-
-/* Used to return the correct title for the double login/logout menu item */
-function bpdm_loginout_title( $title )
-{
-	$titles = explode( '|', $title );
-	if ( ! is_user_logged_in() )
-		return esc_html( isset( $titles[0] ) ? $titles[0] : $title );
-	else
-		return esc_html( isset( $titles[1] ) ? $titles[1] : $title );
-}
-
-/* The main code, this replace the #keyword# by the correct links with nonce ect */
-add_filter( 'wp_setup_nav_menu_item', 'bpdm_setup_nav_menu_item' );
-function bpdm_setup_nav_menu_item( $item )
-{
-	global $pagenow;
-	if( $pagenow!='nav-menus.php' && !defined('DOING_AJAX') && isset( $item->url ) && strstr( $item->url, '#bpdm' ) != '' ){
-		$item_url = substr( $item->url, 0, strpos( $item->url, '#', 1 ) ) . '#';
-		$item_redirect = str_replace( $item_url, '', $item->url );
-		switch( $item_url ) {
-			case '#bpdmloginout#' : 	
-									$item_redirect = explode( '|', $item_redirect );
-									if( count( $item_redirect ) != 2 ) 
-										$item_redirect[1] = $item_redirect[0];
-									for( $i = 0; $i <= 1; $i++ ):
-										if( $item_redirect[$i] == '%actualpage%')
-											$item_redirect[$i] = $_SERVER['REQUEST_URI'];
-									endfor;
-									$item->url = is_user_logged_in() ? wp_logout_url( $item_redirect[1] ) : wp_login_url( $item_redirect[0] );
-									$item->title = bpdm_loginout_title( $item->title ) ; break;
-			case '#bpdmlogin#' : 	$item->url = wp_login_url( $item_redirect ); break;
-			case '#bpdmlogout#' : 	$item->url = wp_logout_url( $item_redirect ); break;
-			case '#bpdmregister#' : 	if( is_user_logged_in() ) $item->title = '#bpdmregister#'; else $item->url = site_url( 'wp-login.php?action=register', 'login' ); break;
-		}
-		$item->url = esc_url( $item->url );
-	}
-	return $item;
-}
-
-add_filter( 'wp_nav_menu_objects', 'bpdm_wp_nav_menu_objects' );
-function bpdm_wp_nav_menu_objects( $sorted_menu_items )
-{
-	foreach( $sorted_menu_items as $k=>$item )
-		if( $item->title==$item->url && $item->title=='#bpdmregister#' )
-			unset( $sorted_menu_items[$k] );
-	return $sorted_menu_items;
-}
-
-/* [login] shortcode */
-add_shortcode( 'login', 'bpdm_shortcode_login' );
-function bpdm_shortcode_login( $atts, $content = null )
-{
-	extract(shortcode_atts(array(
-		"edit_tag" => "",
-		"redirect" => $_SERVER['REQUEST_URI']
-	), $atts ) );
-	$edit_tag = esc_html( strip_tags( $edit_tag ) );
-	$href = wp_login_url( $redirect );
-	$content = $content != '' ? $content : __( 'Log In' );
-	return '<a href="' . esc_url( $href ) . '"' .$edit_tag . '>' . $content . '</a>';
-}
-
-/* [loginout] shortcode */
-add_shortcode( 'loginout', 'bpdm_shortcode_loginout' );
-function bpdm_shortcode_loginout( $atts, $content = null )
-{
-	extract(shortcode_atts(array(
-		"edit_tag" => "",
-		"redirect" => $_SERVER['REQUEST_URI']
-	), $atts ) );
-	$edit_tag = strip_tags( $edit_tag );
-	$href = is_user_logged_in() ? wp_logout_url( $redirect ) : wp_login_url( $redirect );
-	if( $content && strstr( $content, '|' ) != '' ) { // the "|" char is used to split titles
-		$content = explode( '|', $content );
-		$content = is_user_logged_in() ? $content[1] : $content[0];
-	}else{
-		$content = is_user_logged_in() ? __( 'Logout' ) : __( 'Log In' );
-	}
-	return '<a href="' . esc_url( $href ) . '"' .$edit_tag . '>' . $content . '</a>';
-}
-
-/* [logout] shortcode */
-add_shortcode( 'logout', 'bpdm_shortcode_logout' );
-function bpdm_shortcode_logout( $atts, $content = null )
-{
-	extract(shortcode_atts(array(
-		"edit_tag" => "",
-		"redirect" => $_SERVER['REQUEST_URI']
-	), $atts ) );
-	$href = wp_logout_url( $redirect );
-	$edit_tag = esc_html( strip_tags( $edit_tag ) );
-	$content = $content != '' ? $content : __( 'Logout' );
-	return '<a href="' . esc_url( $href ) . '"' .$edit_tag . '>' . $content . '</a>';
-}
-
-/* [register] shortcode */
-add_shortcode( 'register', 'bpdm_shortcode_register' );
-function bpdm_shortcode_register( $atts, $content = null )
-{
-	if( is_user_logged_in() )
-		return '';
-	$href = site_url('wp-login.php?action=register', 'login');
-	$content = $content != '' ? $content : __( 'Register' );
-	$link = '<a href="' . $href. '">' . $content . '</a>';
-	return $link;
+<?php
+if( !defined( 'ABSPATH' ) )
+	die( 'Cheatin\' uh?' );
+
+/* Used to return the correct title for the double login/logout menu item */
+function bpdm_loginout_title( $title )
+{
+	$titles = explode( '|', $title );
+	if ( ! is_user_logged_in() )
+		return esc_html( isset( $titles[0] ) ? $titles[0] : $title );
+	else
+		return esc_html( isset( $titles[1] ) ? $titles[1] : $title );
+}
+
+/* The main code, this replace the #keyword# by the correct links with nonce ect */
+add_filter( 'wp_setup_nav_menu_item', 'bpdm_setup_nav_menu_item' );
+function bpdm_setup_nav_menu_item( $item )
+{
+	global $pagenow;
+
+	if( $pagenow!='nav-menus.php' && !defined('DOING_AJAX') && isset( $item->url ) && strstr( $item->url, '#bpdm' ) != '' ){
+		$item_url = substr( $item->url, 0, strpos( $item->url, '#', 1 ) ) . '#';
+		$item_redirect = str_replace( $item_url, '', $item->url );
+		switch( $item_url ) {
+			case '#bpdmloginout#' : 	
+									$item_redirect = explode( '|', $item_redirect );
+									if( count( $item_redirect ) != 2 ) 
+										$item_redirect[1] = $item_redirect[0];
+									for( $i = 0; $i <= 1; $i++ ):
+										if( $item_redirect[$i] == '%actualpage%')
+											$item_redirect[$i] = $_SERVER['REQUEST_URI'];
+									endfor;
+									$item->url = is_user_logged_in() ? wp_logout_url( $item_redirect[1] ) : wp_login_url( $item_redirect[0] );
+									$item->title = bpdm_loginout_title( $item->title ) ; break;
+			case '#bpdmlogin#' : 	$item->url = wp_login_url( $item_redirect ); break;
+			case '#bpdmlogout#' : 	$item->url = wp_logout_url( $item_redirect ); break;
+			case '#bpdmregister#' : 	if( is_user_logged_in() ) $item->title = '#bpdmregister#'; else $item->url = site_url( 'wp-login.php?action=register', 'login' ); break;
+			case '#bpdmcustom#' : 	$item->url = bp_loggedin_user_domain() . ltrim( $item_redirect, "/" ); break;
+		}
+
+		$item->url = esc_url( $item->url );
+	}
+
+	return $item;
+}
+
+add_filter( 'wp_nav_menu_objects', 'bpdm_wp_nav_menu_objects' );
+function bpdm_wp_nav_menu_objects( $sorted_menu_items )
+{
+	foreach( $sorted_menu_items as $k=>$item )
+		if( $item->title==$item->url && $item->title=='#bpdmregister#' )
+			unset( $sorted_menu_items[$k] );
+	return $sorted_menu_items;
+}
+
+/* [bpdm_login] shortcode */
+add_shortcode( 'bpdm_login', 'bpdm_shortcode_login' );
+function bpdm_shortcode_login( $atts, $content = null )
+{
+	extract(shortcode_atts(array(
+		"edit_tag" => "",
+		"redirect" => $_SERVER['REQUEST_URI']
+	), $atts ) );
+	$edit_tag = esc_html( strip_tags( $edit_tag ) );
+	$href = wp_login_url( $redirect );
+	$content = $content != '' ? $content : __( 'Log In' );
+	return '<a href="' . esc_url( $href ) . '"' .$edit_tag . '>' . $content . '</a>';
+}
+
+/* [bpdm_loginout] shortcode */
+add_shortcode( 'bpdm_loginout', 'bpdm_shortcode_loginout' );
+function bpdm_shortcode_loginout( $atts, $content = null )
+{
+	extract(shortcode_atts(array(
+		"edit_tag" => "",
+		"redirect" => $_SERVER['REQUEST_URI']
+	), $atts ) );
+	$edit_tag = strip_tags( $edit_tag );
+	$href = is_user_logged_in() ? wp_logout_url( $redirect ) : wp_login_url( $redirect );
+	if( $content && strstr( $content, '|' ) != '' ) { // the "|" char is used to split titles
+		$content = explode( '|', $content );
+		$content = is_user_logged_in() ? $content[1] : $content[0];
+	}else{
+		$content = is_user_logged_in() ? __( 'Logout' ) : __( 'Log In' );
+	}
+	return '<a href="' . esc_url( $href ) . '"' .$edit_tag . '>' . $content . '</a>';
+}
+
+/* [bpdm_logout] shortcode */
+add_shortcode( 'logout', 'bpdm_shortcode_logout' );
+function bpdm_shortcode_logout( $atts, $content = null )
+{
+	extract(shortcode_atts(array(
+		"edit_tag" => "",
+		"redirect" => $_SERVER['REQUEST_URI']
+	), $atts ) );
+	$href = wp_logout_url( $redirect );
+	$edit_tag = esc_html( strip_tags( $edit_tag ) );
+	$content = $content != '' ? $content : __( 'Logout' );
+	return '<a href="' . esc_url( $href ) . '"' .$edit_tag . '>' . $content . '</a>';
+}
+
+/* [bpdm_register] shortcode */
+add_shortcode( 'register', 'bpdm_shortcode_register' );
+function bpdm_shortcode_register( $atts, $content = null )
+{
+	if( is_user_logged_in() )
+		return '';
+	$href = site_url('wp-login.php?action=register', 'login');
+	$content = $content != '' ? $content : __( 'Register' );
+	$link = '<a href="' . $href. '">' . $content . '</a>';
+	return $link;
+}
+
+/* [bpdm_custom] shortcode */
+add_shortcode( 'bpdm_custom', 'bpdm_shortcode_custom' );
+function bpdm_shortcode_custom( $atts, $content = null )
+{
+	extract(shortcode_atts(array(
+		"edit_tag" => "",
+		"url_part" => ""
+	), $atts ) );
+	$edit_tag = esc_html( strip_tags( $edit_tag ) );
+	$href = bp_loggedin_user_domain() . ltrim( $url_part, "/" ) ;
+	$content = $content != '' ? $content : __( 'Profile' );
+	return '<a href="' . esc_url( $href ) . '"' .$edit_tag . '>' . $content . '</a>';
 }
